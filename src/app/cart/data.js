bfaf9f6254144e1182c7f3fa5069854ec1997d62
coderjins <coderjins@gmail.com>
2023-12-01 19:13:10 +0900

    component 연습하기(with. props)

---
+++ src/app/cart/data.js
@@ -0,0 +1,5 @@
+let age = 20;
+let name = " YOO ";
+let hi = <h1>안녕하세요</h1>;
+
+export { age, name, hi };
--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,17 +1,32 @@
+import { age, name, hi } from "./data.js";
+
 export default function Cart() {
+  let 장바구니 = ["맥북 AIR", "맥북 PRO", "맥 STUDIO"];
   return (
     <div>
+      {hi}
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      {장바구니.map((product) => {
+        return <CartItem 상품명={product} />;
+      })}
+
+      <BasicButton color="blue" />
     </div>
   );
 }
+const BasicButton = ({ color }) => {
+  return <button className={`basicButton ${color}`}>버튼</button>;
+};
+const CartItem = ({ 상품명 }) => {
+  return (
+    <div className="cart-item">
+      <p>{상품명}</p>
+      <p>$40</p>
+      <p>1개</p>
+      <p>
+        {age}
+        {name}
+      </p>
+    </div>
+  );
+};
