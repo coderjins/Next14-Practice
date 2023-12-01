3a4f68b423c11be8550ccf9366e8810ceea68f67
유진서 <146639849+coderjins@users.noreply.github.com>
2023-12-01 16:57:37 +0900

    메인페이지 직접 꾸며보기, 라우트 하는법, map 함수 활용
    * 메인페이지 직접 꾸며보기
    
    * 라우트 하는법, map 함수 활용

---
+++ src/app/cart/page.js
@@ -0,0 +1,13 @@
+export default function Cart() {
+  return (
+    <div>
+      <h4 className="title">장바구니</h4>
+      <div className="cart">
+        <h4>담은 목록</h4>
+      </div>
+      <div className="cart">
+        <h4>총 금액</h4>
+      </div>
+    </div>
+  );
+}
